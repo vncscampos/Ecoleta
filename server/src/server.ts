2cbcb2efc86d831ccb0c9fb98ed1bd28766ff2ad
vncscampos <viniolic2018@gmail.com>
2020-06-02 11:40:59 -0300

    aula 2

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,9 +1,14 @@
 import express from 'express';
+import path from 'path';
+import cors from 'cors';
+
+import routes from './routes';
 
 const app = express();
+app.use(cors());
+app.use(express.json());
+app.use(routes);
 
-app.get('/users', (req, res) => {
-    return res.json({message: 'Hello World'});
-});
+app.use('/uploads', express.static(path.resolve(__dirname, '..', 'uploads')));
 
 app.listen(3333);
